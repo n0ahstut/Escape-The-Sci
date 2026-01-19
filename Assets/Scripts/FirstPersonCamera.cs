307ec1cc67d8488f29686026a9787face036d58c
n0ahstut <stutmann@bxscience.edu>
2026-01-19 18:26:02 -0500

    Update FirstPersonCamera.cs

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _mouseSensitivity = 2f;
+    [SerializeField] private float _cameraHeight = 0.6f;
+
+    private Camera _camera;
+    private float _verticalRotation = 0f;
+
     void Start()
     {
-        
+        // Create camera as child of player, or find existing main camera
+        _camera = Camera.main;
+        if (_camera == null)
+        {
+            GameObject camObj = new GameObject("FirstPersonCamera");
+            _camera = camObj.AddComponent<Camera>();
+        }
+
+        // Parent camera to player and position at head height
+        _camera.transform.SetParent(transform);
+        _camera.transform.localPosition = new Vector3(0, _cameraHeight, 0);
+        _camera.transform.localRotation = Quaternion.identity;
+
+        // Lock and hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        // Horizontal rotation - rotate the player
+        transform.Rotate(0, mouseX, 0);
+
+        // Vertical rotation - rotate only the camera
+        _verticalRotation -= mouseY;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -90f, 90f);
+        _camera.transform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0);
+
+        // Press Escape to unlock cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
